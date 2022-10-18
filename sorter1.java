72a06eaa15e2f10261ce71d9afd841ce7c2fd7f3
essa122 <esa125@hotmail.com>
2022-10-19 00:52:43 +0300

    add for loop

--- sorter1.java
+++ sorter1.java
@@ -3,5 +3,12 @@ package swe;
 public class sorter1 {
     public static void main(String[] args) {
         System.out.println("Hi Java");
+        // System.out.println("hiiiiiiii");
+        for (int i=0 ; i<=3 ; i++){
+            int z = 0+ i;
+            System.out.println(z);
+        }
+        int x = 1;
+        System.out.println(x);
     }
 }
